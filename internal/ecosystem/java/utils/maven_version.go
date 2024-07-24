06ddb71ea913b97a4b3f0b9977044776a14b0edc
Seal <team@seal.security>
2024-07-24 19:13:13 +0000

    Version v0.3.52

--- internal/ecosystem/java/utils/maven_version.go
+++ internal/ecosystem/java/utils/maven_version.go
@@ -1,6 +1,7 @@
 package utils
 
 import (
+	"bufio"
 	"cli/internal/common"
 	"log/slog"
 	"strings"
@@ -25,15 +26,28 @@ func GetVersion(targetDir string) string {
 	return version
 }
 
+// "3.6.3\x1b[m\nMaven"
 func parseMavenVersion(mavenVersionOutput string) string {
-	splitted := strings.Split(mavenVersionOutput, " Maven ")
+	r := bufio.NewReader(strings.NewReader(mavenVersionOutput))
+
+	scanner := bufio.NewScanner(r) // handles line-endings correctly per os
+	scanner.Scan()
+	line := scanner.Text()
+	line = strings.Split(line, "\r")[0] // in case for some reason we parase windows output on non windows machine
+	splitted := strings.Split(line, " Maven ")
+
 	if len(splitted) < 2 {
 		slog.Error("failed parsing maven version", "output", mavenVersionOutput)
 		return ""
 	}
 
 	versionWithSuffix := splitted[1]
-	spaceIndex := strings.Index(versionWithSuffix, " ")
-	version := versionWithSuffix[:spaceIndex]
-	return version
+
+	// strip color escape code if exists https://en.wikipedia.org/wiki/ANSI_escape_code#Colors
+	if colorIdx := strings.Index(versionWithSuffix, "\x1b"); colorIdx != -1 {
+		// color code escape character
+		versionWithSuffix = versionWithSuffix[:colorIdx]
+	}
+
+	return strings.Split(versionWithSuffix, " ")[0]
 }
