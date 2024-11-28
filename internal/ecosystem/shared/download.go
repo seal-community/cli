b5b91009fce53e38852275e283bfaf74d0a16295
Seal <team@seal.security>
2024-11-28 11:25:58 +0000

    Version v0.3.100

--- internal/ecosystem/shared/download.go
+++ internal/ecosystem/shared/download.go
@@ -10,22 +10,22 @@ import (
 func DownloadFile(s api.ArtifactServer, uri string) ([]byte, error) {
 	defer common.ExecutionTimer().Log()
 
-	libraryData, statusCode, err := s.Get(uri, nil, nil)
+	fileData, statusCode, err := s.Get(uri, nil, nil)
 
 	if err != nil {
-		slog.Error("failed sending request for library data using", "err", err, "uri", uri)
+		slog.Error("failed sending request for file data using", "err", err, "uri", uri)
 		return nil, err
 	}
 
 	if statusCode != 200 {
-		slog.Error("bad response code for golang package payload", "err", err, "status", statusCode)
-		return nil, fmt.Errorf("bad status code golang package data; status: %d", statusCode)
+		slog.Error("bad response code for file payload", "err", err, "status", statusCode, "uri", uri)
+		return nil, fmt.Errorf("bad status code for file data; status: %d", statusCode)
 	}
 
-	if len(libraryData) == 0 {
+	if len(fileData) == 0 {
 		slog.Error("no payload content from server")
-		return nil, fmt.Errorf("no package content")
+		return nil, fmt.Errorf("no file content")
 	}
 
-	return libraryData, nil
+	return fileData, nil
 }
